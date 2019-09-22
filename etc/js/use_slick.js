8c6d82e678f4a0d6fb8d7dbdb5b7eb786b8ae719
Robot-Inventor <50550561+Robot-Inventor@users.noreply.github.com>
2019-09-22 16:45:22 +0900

    Update use_slick.js

--- etc/js/use_slick.js
+++ etc/js/use_slick.js
@@ -1,8 +1,8 @@
 $(document).ready(function(){
     $('.slide').slick({
-        adaptiveHeight: true
-        autoplay: true
-        fade: true
+        adaptiveHeight: true,
+        autoplay: true,
+        fade: true,
         focusOnSelect: true
     });
 });
