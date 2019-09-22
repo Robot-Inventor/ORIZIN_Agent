b000f38b3d0f0d287cbc560a6ae172b4c19502d8
Robot-Inventor <50550561+Robot-Inventor@users.noreply.github.com>
2019-09-22 18:01:09 +0900

    Update use_slick.js

--- etc/js/use_slick.js
+++ etc/js/use_slick.js
@@ -3,7 +3,6 @@ $(document).ready(function(){
         autoplay: true,
      　　adaptiveHeight: true,
      　　arrows: true,
-        rtl: true,
     　 　prevArrow:'<div class="prev">PREV</div>',
  　    　nextArrow:'<div class="next">NEXT</div>'
     });
